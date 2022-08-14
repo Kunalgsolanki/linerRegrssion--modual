218006df835e6bbf231e2c27db82d4f1d3842238
Kunal <kgs2002107@gmail.com>
2022-08-14 11:54:53 +0530

    kunal

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"kgs2002107@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
